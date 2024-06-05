eb2777a7675f10a65b7ee904dae1300628628221
Luiz Karat <luiz.karat.silva@gmail.com>
2024-06-05 08:58:51 -0300

    exercício 2 lacos 2

--- script.js
+++ script.js
@@ -57,11 +57,26 @@ function lerNotas() {
         soma = 0
     }
 }
-lerNotas()
 
+function lerNotasForIn() {
+    const notasDoano = [
+        [8, 6, 9, 2],
+        [7, 5, 4, 8],
+        [2, 3, 8, 9],
+        [7, 1, 9, 8]
+    ]
+    let soma = 0;
+    for (let nBimestre in notasDoano) {
+        for (notas = 0; notas < notasDoano[nBimestre].length; notas++) {
+            console.log(notasDoano[nBimestre][notas]);
+            soma += notasDoano[nBimestre][notas]
+        }
+        console.log(`A soma das notas do bimestre ${nBimestre + 1} é: ${soma} e a média do bimestre ${nBimestre + 1} é ${soma / notasDoano[nBimestre].length}`);
+        soma = 0
+    }
+}
 
-
-
+lerNotasForIn()
 
 
 
