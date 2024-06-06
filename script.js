3411f3086d4abf6fe90f58fbb14ddedfd94d0e2e
Luiz Karat <luiz.karat.silva@gmail.com>
2024-06-06 09:12:14 -0300

    exercicio danado

--- script.js
+++ script.js
@@ -94,8 +94,58 @@ function lerNotasForOf() {
         soma = 0
     }
 }
-lerNotasForOf()
 
+const filmes = [
+    {
+        titulo: 'O Auto da Compadecida',
+        ano: 2000,
+        diretor: 'Guel Arraes',
+        elenco: [
+            'Selton Mello',
+            'Mateus Nachtergaele',
+            'Marco Nanini',
+            'Fernanda Montenegro',
+        ],
+    },
+    {
+        titulo: 'Carandiru',
+        ano: 2001,
+        diretor: 'Hector Babenco',
+        elenco: [
+            'Wagner Moura',
+            'José Carlos Vasconcelos',
+            'Ailton Graça',
+            'Caio Blat',
+        ],
+    },
+    {
+        titulo: 'Aquarius',
+        ano: 2012,
+        diretor: 'Kléber Mendonça Filho',
+        elenco: [
+            'Sônia Braga',
+            'Humberto Carrão',
+            'Maeve Jinkings',
+            'Bárbara Colen',
+        ],
+    },
+]
+
+// escreva seu código abaixo
+
+for (let index = 0; index < filmes.length; index++) {
+    const titulo = filmes[index].titulo
+    const ano = filmes[index].ano
+    const diretor = filmes[index].diretor
+    const elenco = filmes[index].elenco
+
+    for (let i = 0; i < elenco.length; i++) {
+        
+        
+    }
+console.log(`${titulo}, de ${ano}, dirigido por ${diretor}, e elenco ${elenco}`);
+
+}
 
 
 
